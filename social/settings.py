1c49e2ab83f6543ca4b0cb50f6a2ce95e6aca882
Oluwatimilehin2004 <oluwatimilehinayo2004@gmail.com>
2025-01-30 14:17:57 -0600

    configured AWS bucket for rendering images

--- social/settings.py
+++ social/settings.py
@@ -169,9 +169,9 @@ CHANNEL_LAYERS = {
 }
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = 'your-access-key-id'
-AWS_SECRET_ACCESS_KEY = 'your-secret-access-key'
-AWS_STORAGE_BUCKET_NAME = 'your-bucket-name'
-AWS_S3_REGION_NAME = 'your-region'  # e.g., 'us-east-1'
+AWS_ACCESS_KEY_ID = 'AKIAZPPGABQYMPWNTJP4'
+AWS_SECRET_ACCESS_KEY = 'K5mLleKG7KpSoJUeSBFVQRZ6fO6ivJ6sRK3cF628'  
+AWS_STORAGE_BUCKET_NAME = 'linksphere-media'
+AWS_S3_REGION_NAME = 'us-east-1'  # e.g., 'us-east-1'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
