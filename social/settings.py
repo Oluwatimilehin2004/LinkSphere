0e47aa0a57a451fa58aa93941fa2919c98fdffbb
Oluwatimilehin2004 <oluwatimilehinayo2004@gmail.com>
2025-01-30 04:25:11 -0600

    thrid update: deleted django default database

--- social/settings.py
+++ social/settings.py
@@ -86,10 +86,6 @@ ASGI_APPLICATION = 'social.asgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    },
     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
